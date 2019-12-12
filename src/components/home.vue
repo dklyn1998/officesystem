081a714ce8dc95267be8d14d4c514145c607a3b4
Your Name <dk19980308@qq.com>
2019-12-12 13:39:44 +0800

    '加入了字体图标'

--- src/components/home.vue
+++ src/components/home.vue
@@ -69,7 +69,7 @@
           <el-header>
             <img src="../assets/logo.png" class='logo_top' alt="">
             <div class="div_left">管理员</div>
-            <el-button>退出</el-button>
+            <el-button icon="el-icon-circle-close">退出</el-button>
           </el-header>
           <!-- 页面主体区域 -->
           <el-main><router-view></router-view></el-main>
